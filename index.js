448314ff6029f2af3836bf162d7e506d128519da
Jaehooni <8362532@gmail.com>
2023-11-30 18:52:17 +0900

    Docs: Add modules

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 import cors from 'cors';
 import { status } from './config/response.status.js';
-// import { BaseError } from './config/error.js';
-// import { tempRouter } from './src/routes/temp.route.js';
 import { response } from './config/response.js';
 import { specs } from './config/swagger.config.js';
 import SwaggerUi from 'swagger-ui-express';
+// import { BaseError } from './config/error.js';
+// import { tempRouter } from './src/routes/temp.route.js';
 
 const app = express();
 
@@ -18,19 +19,19 @@ app.use(express.urlencoded({extended: false}));
 // swagger
 app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(specs));
 
-// app.use((req, res, next) => {
-//     res.locals.message = err.message;
-//     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-//     console.log("error", err);
-//     res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
-// });
-
 app.use((err, req, res, next) => {
     res.locals.message = err.message;   
     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
     console.log("error", err);
     res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
 });
+
+// app.use((req, res, next) => {
+//     res.locals.message = err.message;
+//     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+//     console.log("error", err);
+//     res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
+// });
 
 app.listen(app.get('port'), () => {
     console.log(`Example app listening on port ${app.get('port')}`)
