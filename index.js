4cf91fda2b7fbf7bd1a0d299e5dc65553cbeccfc
Jaehooni <8362532@gmail.com>
2023-11-30 18:42:47 +0900

    Docs: Add config files(DB, Swagger)

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 import express from 'express';
+import cors from 'cors';
 import { status } from './config/response.status.js';
-import { BaseError } from './config/error.js';
-import { tempRouter } from './src/routes/temp.route.js';
+// import { BaseError } from './config/error.js';
+// import { tempRouter } from './src/routes/temp.route.js';
 import { response } from './config/response.js';
+import { specs } from './config/swagger.config.js';
+import SwaggerUi from 'swagger-ui-express';
 
 const app = express();
-const port = 3000;
 
-app.use('/temp', tempRouter);
+app.set('port', process.env.PORT || 3000);
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
-    const err = new BaseError(status.BAD_REQUEST);
-    next(err);
-});
+// swagger
+app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(specs));
+
+// app.use((req, res, next) => {
+//     res.locals.message = err.message;
+//     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+//     console.log("error", err);
+//     res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
+// });
 
 app.use((err, req, res, next) => {
     res.locals.message = err.message;   
     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
-    res.status(err.data.status).send(response(err.data));
+    console.log("error", err);
+    res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(app.get('port'), () => {
+    console.log(`Example app listening on port ${app.get('port')}`)
 });
 
